7e0ba002f262d834b79fb52a02817b8b7348cd03
JasonNelemans <jasonnelemans@hotmail.com>
2020-11-04 12:21:32 +0100

    created and implemented PokemonAttributes component

--- src/components/PokemonDetail.vue
+++ src/components/PokemonDetail.vue
@@ -8,37 +8,7 @@
           :height="'220'" 
         />
       </div>
-      <div v-if="pokemon" class="data">
-        <h2>{{ pokemon.name }}</h2>
-        <div class="property">
-          <div class="left">Base Experience</div>
-          <div class="right">{{ pokemon.base_experience }} XP</div>
-        </div>
-        <div class="property">
-          <div class="left">Height</div>
-          <div class="right">{{ pokemon.height / 10 }} m</div>
-        </div>
-        <div class="property">
-          <div class="left">Weight</div>
-          <div class="right">{{ pokemon.weight / 10 }} kg</div>
-        </div>
-        <h3>Pokemon Types</h3>
-        <div class="types">
-          <div class="type" 
-            v-for="(value, index) in pokemon.types"
-            :key="'value'+index">
-            {{ value.type.name }}
-          </div>
-        </div>
-        <h3>Abilities</h3>
-        <div class="abilities">
-          <div class="ability" 
-            v-for="(value, index) in pokemon.abilities"
-            :key="'value'+index">
-            {{ value.ability.name }}
-          </div>
-        </div>
-      </div>
+      <PokemonAttributes :pokemon="pokemon" v-if="pokemon" />
       <div class="detail-buttons">
         <router-link to="/"><button class="home-button">Back to Home</button></router-link>
         <router-link to="/random"><button class="random-button" @click="randomizeId">Randomize</button></router-link>
@@ -56,12 +26,14 @@
 <script lang="ts">
   import RelatedPokemon from "@/components/RelatedPokemon.vue"
   import PokemonImage from "@/components/PokemonImage.vue"
+  import PokemonAttributes from "@/components/PokemonDetails/PokemonAttributes.vue"
 
   export default {
     name: "pokemon-detail",
     components: {
       RelatedPokemon,
-      PokemonImage
+      PokemonImage,
+      PokemonAttributes
     },
     props: {
       id: String
@@ -148,54 +120,6 @@
 
       h2 {
         text-transform: capitalize;
-      }
-
-      .data {
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-        flex-direction: column;
-        width: 100%;
-        margin-bottom: 40px;
-
-        .property {
-          width: 90%;
-          max-width: 400px;
-          border-bottom: 1px solid #ccc;
-          margin-bottom: 10px;
-          
-          .left { float: left; }
-          .right { float: right; }
-        }
-
-        h3 {
-          width: 90%;
-          max-width: 400px;
-          border-bottom: 1px solid #ccc;
-        }
-
-        .types, .abilities {
-          display: flex;
-          justify-content: flex-start;
-          flex-wrap: wrap;
-          width: 90%;
-          max-width: 400px;
-
-          .type, .ability {
-            margin: 0 10px 10px 0;
-            padding: 5px 10px;
-            border-radius: 20px;
-            color: #fff;
-            font-size: 1rem;
-            letter-spacing: 2px;
-            text-transform: capitalize;
-            word-wrap: none;
-            word-break: keep-all;
-          }
-
-          .type { background-color: #0A2E50; }
-          .ability { background-color: #C73015; }
-        }
       }
 
       .random-button {
---
+++ src/components/PokemonDetails/PokemonAttributes.vue
@@ -0,0 +1,91 @@
+<template>
+  <div v-if="pokemon" class="attributes">
+    <h2>{{ pokemon.name }}</h2>
+    <div class="property">
+      <div class="left">Base Experience</div>
+      <div class="right">{{ pokemon.base_experience }} XP</div>
+    </div>
+    <div class="property">
+      <div class="left">Height</div>
+      <div class="right">{{ pokemon.height / 10 }} m</div>
+    </div>
+    <div class="property">
+      <div class="left">Weight</div>
+      <div class="right">{{ pokemon.weight / 10 }} kg</div>
+    </div>
+    <h3>Pokemon Types</h3>
+    <div class="types">
+      <div class="type" 
+        v-for="(value, index) in pokemon.types"
+        :key="'value'+index">
+        {{ value.type.name }}
+      </div>
+    </div>
+    <h3>Abilities</h3>
+    <div class="abilities">
+      <div class="ability" 
+        v-for="(value, index) in pokemon.abilities"
+        :key="'value'+index">
+        {{ value.ability.name }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      pokemon: Object
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  .attributes {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    margin-bottom: 40px;
+
+    .property {
+      width: 90%;
+      max-width: 400px;
+      border-bottom: 1px solid #ccc;
+      margin-bottom: 10px;
+      
+      .left { float: left; }
+      .right { float: right; }
+    }
+
+    h3 {
+      width: 90%;
+      max-width: 400px;
+      border-bottom: 1px solid #ccc;
+    }
+
+    .types, .abilities {
+      display: flex;
+      justify-content: flex-start;
+      flex-wrap: wrap;
+      width: 90%;
+      max-width: 400px;
+
+      .type, .ability {
+        margin: 0 10px 10px 0;
+        padding: 5px 10px;
+        border-radius: 20px;
+        color: #fff;
+        font-size: 1rem;
+        letter-spacing: 2px;
+        text-transform: capitalize;
+        word-wrap: none;
+        word-break: keep-all;
+      }
+
+      .type { background-color: #0A2E50; }
+      .ability { background-color: #C73015; }
+    }
+  }
+</style>
